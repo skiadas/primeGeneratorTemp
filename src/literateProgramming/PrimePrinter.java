137dab0026dc6cc84307569198174b63e6f1e564
Haris Skiadas <cskiadas@gmail.com>
2021-09-20 20:13:15 -0400

    Extract generatePrimes and printNumbers methods

--- src/literateProgramming/PrimePrinter.java
+++ src/literateProgramming/PrimePrinter.java
@@ -3,17 +3,46 @@ package literateProgramming;
 
 public class PrimePrinter {
     public static void main(String[] args) {
-        int candidatePrime = 1;
-        int lastPrimeIndex = 1;
         final int numPrimes = 1000;
+        int[] primes = generatePrimes(numPrimes);
+        printNumbers(numPrimes, primes);
+    }
+
+    private static void printNumbers(int numPrimes, int[] primes) {
+        int pageNumber = 1;
+        int pageOffset = 1;
+        final int rowsPerPage = 50;
+        final int columnsPerPage = 4;
+        while (pageOffset <= numPrimes) {
+            System.out.print("The First ");
+            System.out.print(Integer.toString(numPrimes));
+            System.out.print(" Prime Numbers --- Page ");
+            System.out.print(Integer.toString(pageNumber));
+            System.out.println("\n");
+            for (int rowOffset = pageOffset; rowOffset <= pageOffset + rowsPerPage - 1; rowOffset++) {
+                for (int col = 0; col <= columnsPerPage - 1; col++) {
+                    if (rowOffset + col * rowsPerPage <= numPrimes) {
+                        System.out.printf("%10d", primes[rowOffset + col * rowsPerPage]);
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("\f");
+            pageNumber++;
+            pageOffset += rowsPerPage * columnsPerPage;
+        }
+    }
+
+    private static int[] generatePrimes(int numPrimes) {
         int[] primes = new int[numPrimes + 1];
         primes[1] = 2;
         int ORD = 2;
         int nextPrimeSquare = 9;
-
         int N = 0;
         final int ORDMAX = 30;
         int[] multiples = new int[ORDMAX + 1];
+        int candidatePrime = 1;
+        int lastPrimeIndex = 1;
         while (lastPrimeIndex < numPrimes) {
             boolean possiblyPrime;
             do {
@@ -38,27 +67,6 @@ public class PrimePrinter {
             lastPrimeIndex++;
             primes[lastPrimeIndex] = candidatePrime;
         }
-        int pageNumber = 1;
-        int pageOffset = 1;
-        final int rowsPerPage = 50;
-        final int columnsPerPage = 4;
-        while (pageOffset <= numPrimes) {
-            System.out.print("The First ");
-            System.out.print(Integer.toString(numPrimes));
-            System.out.print(" Prime Numbers --- Page ");
-            System.out.print(Integer.toString(pageNumber));
-            System.out.println("\n");
-            for (int rowOffset = pageOffset; rowOffset <= pageOffset + rowsPerPage - 1; rowOffset++) {
-                for (int col = 0; col <= columnsPerPage - 1; col++) {
-                    if (rowOffset + col * rowsPerPage <= numPrimes) {
-                        System.out.printf("%10d", primes[rowOffset + col * rowsPerPage]);
-                    }
-                }
-                System.out.println();
-            }
-            System.out.println("\f");
-            pageNumber++;
-            pageOffset += rowsPerPage * columnsPerPage;
-        }
+        return primes;
     }
 }
